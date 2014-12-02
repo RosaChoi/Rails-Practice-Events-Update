2ae867b5942f29b29804cc52a46aa8f5df4b6159
waselrosa <rosa@waselrosa.com>
2014-12-02 13:31:07 -0800

    CRUD exercise - created new, create, edit from scratch

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,45 @@
+class EventsController < ApplicationController
+
+  def new
+    @event = Event.new
+  end
+
+  def index
+    @events = Event.all
+  end
+
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to events_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+
+    if @event.update(event_params)
+      redirect_to events_path
+    else
+      render :edit
+    end
+  end
+
+  private
+  def event_params
+    params.require(:event).permit(
+    :date,
+    :description,
+    :requires_id,
+    :capacity,
+    :directions,
+    )
+  end
+
+end
